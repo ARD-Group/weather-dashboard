ff18179c27e6f2d486fcf1995ccbc03a36c4989d
Deyaa Pozan <dpozan@connectedmotion.io>
2025-05-01 01:27:40 +0300

    feat: make header responsive but need some improve

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,19 @@ import React from "react";
 
 import { ThemeToggle } from "../ThemeToggle";
 import { MultipleSelector } from "../../web-building-blocks/Atoms/MultipleSelector";
-import { Locate, Search } from "lucide-react";
 import { Button } from "../../web-building-blocks/Atoms";
+import { BiCurrentLocation } from "react-icons/bi";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Header: React.FC = () => {
   return (
-    <header className="flex items-center justify-between p-6 bg-gradient-to-b ">
+    <header className="flex flex-col sm:flex-row items-center justify-between p-4 sm:p-6 bg-gradient-to-b gap-4 sm:gap-0">
       {/* Dark Mode Toggle */}
-      <div className="flex flex-col items-center mr-4">
+      <div className="flex flex-col items-center sm:mr-4">
         <ThemeToggle />
       </div>
       {/* Search Bar */}
-      <div className="mx-4 w-[803px]">
+      <div className="w-full sm:w-[60%] lg:w-[50%]">
         <MultipleSelector
           mode="single"
           emptyIndicator="No results found"
@@ -24,23 +25,25 @@ const Header: React.FC = () => {
             { label: "React", value: "react" },
           ]}
           inputPlaceholder="Search for your preferred city..."
-          className="search-bar flex items-center px-4 py-2 rounded-full bg-search-bg shadow-button w-full  mx-auto text-search-text placeholder-search-placeholder text-sm outline-none border-none"
+          className="search-bar flex items-center px-4 py-2 rounded-full bg-search-bg shadow-button w-full mx-auto text-search-text placeholder-search-placeholder text-sm outline-none border-none"
           styleClasses={{
             searchInputProps:
-              "bg-transparent w-full text-search-text placeholder-search-placeholder text-sm  outline-none border-none",
+              "bg-transparent w-full text-search-text placeholder-search-placeholder text-sm outline-none border-none",
           }}
-          icon={<Search className="h-5 w-5" />}
-          mainIcon={<Search className="h-[46px] w-[48px] " />}
+          icon={<IoSearchOutline className="h-5 w-5" />}
+          mainIcon={<IoSearchOutline className="h-[46px] w-[48px]" />}
         />
       </div>
       {/* Current Location Button */}
       <Button
         dataTestId="button-1"
-        buttonStyle="current-location-btn w-[292px] h-[62px] flex items-center gap-2 px-6 py-2 rounded-full bg-current-location-bg text-current-location-text font-semibold text-base shadow-button hover:bg-green-600  transition-colors duration-200"
+        buttonStyle="current-location-btn w-full sm:w-[200px] lg:w-[292px] h-[62px] flex items-center justify-center gap-2 px-4 sm:px-6 py-2 rounded-full bg-current-location-bg text-current-location-text font-semibold text-base shadow-button hover:bg-green-600 transition-colors duration-200"
         onClick={() => {}}
       >
-        <Locate className="h-5 w-5" />
-        Current Location
+        <React.Fragment key=".0">
+          <BiCurrentLocation className="h-[35px] w-[35px]" />
+          <p className="hidden sm:block">Current Location</p>
+        </React.Fragment>
       </Button>
     </header>
   );
